450a177f57aa78205ac00b06c0021a3390dd6834
iperezx <i3perez@sdsc.edu>
2022-08-24 09:23:34 -0700

    SAGE-1423 Virtual nodes on dev cloud
    Add beekeeper api endpoint, and registration host and port as build args and env

--- vm/entrypoint.sh
+++ vm/entrypoint.sh
@@ -19,15 +19,15 @@ cp ./sage_registration-cert.pub_readonly sage_registration-cert.pub
 cp ./sage_registration_readonly sage_registration
 
 # prepare ssh_known_hosts
-echo '@cert-authority' bk-api $(cat /etc/waggle/beekeeper_ca_key.pub | cut -f 1,2 -d ' ') > /etc/ssh/ssh_known_hosts
+echo '@cert-authority' ${BK_API_HOST} $(cat /etc/waggle/beekeeper_ca_key.pub | cut -f 1,2 -d ' ') > /etc/ssh/ssh_known_hosts
 
 # wait for the ssh sever
-while ! nc -z bk-sshd 2201; do
+while ! nc -z ${BK_REGISTRATION_HOST} ${BK_REGISTRATION_PORT}; do
   sleep 1
 done
 
 # cert-authority above did not work for some reason, thus we add the host server directly:
-ssh-keyscan -H -p 2201 bk-sshd >> /etc/ssh/ssh_known_hosts
+ssh-keyscan -H -p ${BK_REGISTRATION_PORT} ${BK_REGISTRATION_HOST} >> /etc/ssh/ssh_known_hosts
 
 # start sshd  (usually via systemd)
 mkdir -p /var/run/sshd
